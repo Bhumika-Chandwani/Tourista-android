57d6c79b3293e926aed258071d5fe2933030ca5c
Bhumika Chandwani <bhumikachandwani017@gmail.com>
2026-09-03 14:40:06 +0530

    Torista design system and splash screen.

--- app/src/main/java/com/example/touristguide/activities/SplashActivity.java
+++ app/src/main/java/com/example/touristguide/activities/SplashActivity.java
@@ -1,4 +1,42 @@
 package com.example.touristguide.activities;
 
-public class SplashActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.touristguide.R;
+import com.example.touristguide.utils.FirebaseUtil;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+
+            if (FirebaseUtil.getAuth().getCurrentUser() != null) {
+                openMain();
+            } else {
+                openLogin();
+            }
+
+        }, SPLASH_DELAY);
+    }
+
+    private void openMain() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    private void openLogin() {
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
 }
